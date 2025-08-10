88a4f0185f69be66530a36af3080f3b09bf501c5
MukeshSetti <settimukesh13@gmail.com>
2025-08-10 22:30:13 +0530

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-export { default as Home } from './home.jsx'
+export { default as Home } from './Home.jsx'
 export { default as Orders } from './Orders.jsx'
 
 export { default as Auth } from './Auth.jsx'
